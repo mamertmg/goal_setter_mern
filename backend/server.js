beb1ec92f4709c68ff5b5d479938bfdd64014045
Mamertmg <marquezgarcia.a@gmail.com>
2022-02-20 13:40:08 +0100

    added login and register

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
-const express = require('express');
-const dotenv = require('dotenv').config();
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
-
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
+
+connectDB()
 
 const app = express()
 
@@ -11,6 +14,19 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+  
+    app.get('*', (req, res) =>
+      res.sendFile(
+        path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+      )
+    )
+  } else {
+    app.get('/', (req, res) => res.send('Please set to production'))
+}
 
 app.use(errorHandler)
 
